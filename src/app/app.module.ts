7ce69134b2c82537a7b9cfd80cf4212e99a42719
Gaetano Herman <gaetano.herman.u9781@student.hogent.be>
2017-08-15 00:21:40 +0200

    Eerste upload BH applicatie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductLijstComponent } from './product-lijst/product-lijst.component';
+import { UrenLijstComponent } from './uren-lijst/uren-lijst.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductLijstComponent,
+    UrenLijstComponent
   ],
   imports: [
     BrowserModule
